ecbafb49709cab2e51fa5822c950a241c3efb0d1
lucas <lucassantoscrfbezerra@gmail.com>
2021-01-23 12:19:03 -0300

    Add style home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,37 @@
-import React from "react";
-export default function app() {
-  return <div>home</div>;
+import React, { useState, useEffect } from "react";
+import api from "../../services/http";
+import { MdFlightTakeoff } from "react-icons/md";
+import "./style.css";
+
+export default function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    loadApi();
+  }, []);
+
+  async function loadApi() {
+    const response = await api.get("trips");
+    setTrips(response.data);
+  }
+
+  return (
+    <div>
+      <div className="box">
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? "Disponivel" : "Indisponivel"}</span>
+            <button type="button" onClick={() => {}}>
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF" />
+              </div>
+              <span>SOLICITAR RESERVA</span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 }
